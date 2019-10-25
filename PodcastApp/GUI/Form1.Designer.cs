dba059acffa9658dc3e7862f4129bd14eee3f710
Mattias Bolander <55517764+TiasB@users.noreply.github.com>
2019-10-25 15:19:14 +0200

    Form mattias

--- PodcastApp/GUI/Form1.Designer.cs
+++ PodcastApp/GUI/Form1.Designer.cs
@@ -30,15 +30,25 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnNy1 = new System.Windows.Forms.Button();
+            this.btnSpara1 = new System.Windows.Forms.Button();
+            this.btnTaBort1 = new System.Windows.Forms.Button();
+            this.cboUppdateringsfrekverns = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(18, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 18);
+            this.label1.Size = new System.Drawing.Size(135, 26);
             this.label1.TabIndex = 0;
             this.label1.Text = "Podcasts";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -46,20 +56,110 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 30);
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(18, 46);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(220, 147);
+            this.listBox1.Size = new System.Drawing.Size(487, 184);
             this.listBox1.TabIndex = 1;
+            // 
+            // btnNy1
+            // 
+            this.btnNy1.Location = new System.Drawing.Point(72, 303);
+            this.btnNy1.Name = "btnNy1";
+            this.btnNy1.Size = new System.Drawing.Size(131, 35);
+            this.btnNy1.TabIndex = 2;
+            this.btnNy1.Text = "...Ny";
+            this.btnNy1.UseVisualStyleBackColor = true;
+            // 
+            // btnSpara1
+            // 
+            this.btnSpara1.Location = new System.Drawing.Point(257, 303);
+            this.btnSpara1.Name = "btnSpara1";
+            this.btnSpara1.Size = new System.Drawing.Size(121, 35);
+            this.btnSpara1.TabIndex = 3;
+            this.btnSpara1.Text = "Spara";
+            this.btnSpara1.UseVisualStyleBackColor = true;
+            // 
+            // btnTaBort1
+            // 
+            this.btnTaBort1.Location = new System.Drawing.Point(384, 303);
+            this.btnTaBort1.Name = "btnTaBort1";
+            this.btnTaBort1.Size = new System.Drawing.Size(121, 35);
+            this.btnTaBort1.TabIndex = 4;
+            this.btnTaBort1.Text = "Ta bort";
+            this.btnTaBort1.UseVisualStyleBackColor = true;
+            // 
+            // cboUppdateringsfrekverns
+            // 
+            this.cboUppdateringsfrekverns.FormattingEnabled = true;
+            this.cboUppdateringsfrekverns.Location = new System.Drawing.Point(257, 269);
+            this.cboUppdateringsfrekverns.Name = "cboUppdateringsfrekverns";
+            this.cboUppdateringsfrekverns.Size = new System.Drawing.Size(121, 28);
+            this.cboUppdateringsfrekverns.TabIndex = 6;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(384, 269);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 28);
+            this.comboBox3.TabIndex = 7;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 271);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(233, 26);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "http://";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = ":URL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(257, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = ":Uppdateringsfrekvens";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(406, 239);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "label4";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gainsboro;
-            this.ClientSize = new System.Drawing.Size(570, 292);
+            this.ClientSize = new System.Drawing.Size(1173, 590);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.cboUppdateringsfrekverns);
+            this.Controls.Add(this.btnTaBort1);
+            this.Controls.Add(this.btnSpara1);
+            this.Controls.Add(this.btnNy1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.Text = "PodcastApp";
@@ -73,6 +173,15 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnNy1;
+        private System.Windows.Forms.Button btnSpara1;
+        private System.Windows.Forms.Button btnTaBort1;
+        private System.Windows.Forms.ComboBox cboUppdateringsfrekverns;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
