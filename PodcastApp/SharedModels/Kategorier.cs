f55ad7850ffac199d305365b9766828d26cbe06d
Mattias Bolander <55517764+TiasB@users.noreply.github.com>
2019-10-31 23:06:20 +0100

    kategorier

---
+++ PodcastApp/SharedModels/Kategorier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedModels
+{
+    public class Kategorier
+    {
+        public string KategoriNamn { get; set; }
+        public Kategorier(string kategoriNamn)
+        {
+            this.KategoriNamn = kategoriNamn;
+        }
+    }
+}
