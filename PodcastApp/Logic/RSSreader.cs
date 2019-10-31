21a1ccbb2eb04d2aa2feef6f9b9ec5e68b0aa7a8
Mattias Bolander <55517764+TiasB@users.noreply.github.com>
2019-10-31 08:35:17 +0100

    med RSSreader class

---
+++ PodcastApp/Logic/RSSreader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    class RSSreader
+    {
+        //public static list<podcastshow> getpodcastfeed(string )
+        //{
+        //    var count = 0;
+        //    list<podcastshow> podcastlista = new list<podcastshow>();
+        //    string rssfeedurl = "http://borssnack.libsyn.com/rss";
+        //    using (xmlreader reader = xmlreader.create(rssfeedurl))
+        //    {
+        //        syndicationfeed feed = syndicationfeed.load(reader);
+        //        foreach (syndicationitem item in feed.items)
+        //        {
+                    
+        //            podcastshow nypodd = new podcastshow();
+        //            count++;
+        //            nypodd.title += item.title.text; //funkar
+        //            nypodd.url += item.links[0].uri.originalstring; //funkar
+        //            nypodd.description = item.summary.text; //funkar men tar med <p> taggar
+        //            podcastlista.add(nypodd);
+        //        }
+        //        return podcastlista;
+        //    }
+        //}
+    }
+}
