e78036d42afcf688d0c780317ec5ea2faceb7a84
Mattias Bolander <55517764+TiasB@users.noreply.github.com>
2019-11-03 13:56:24 +0100

    vi kör igen

--- PodcastApp/Data/SerializedReaderWriter.cs
+++ PodcastApp/Data/SerializedReaderWriter.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Data.Interfaces;
+using static Data.Exceptions.SerializerExceptions;
 
 namespace Data
 {
     class SerializedReaderWriter
     {
+        public static void Write<PodcastShow>(string filename, PodcastShow data)
+        {
+            // TODO: Write data.Serialize() into filename using StreamWriter!
+            using (var fs = new FileStream(filename, FileMode.OpenOrCreate))
+            {
+                using (var sw = new StreamWriter(fs))
+                {
+                    sw.Write(data.Serialize());
+                }
+            }
+
+        }
+        public string Serialize()
+        {
+            var lines = new List<string>();
+            foreach (var item in this)
+            {
+                lines.Add(item.Serialize());
+            }
+            return string.Join("\n", lines.ToArray());
+        }
+        public void Deserialize(string serializedString)
+        {
+            var lines = serializedString.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var line in lines)
+            {
+                try
+                {
+                    Add(SerializeableFactory.FromString<T>(line));
+                }
+                catch (Exception ex)
+                {
+                    throw ex as DeserializationException;
+                }
+            }
+        }
     }
 }
