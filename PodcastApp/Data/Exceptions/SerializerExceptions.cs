0c7f5c4be88a6c9b7352857c7e955078be5e7a40
Mattias Bolander <55517764+TiasB@users.noreply.github.com>
2019-11-03 13:39:09 +0100

    extra musmatta

---
+++ PodcastApp/Data/Exceptions/SerializerExceptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Exceptions
+{
+    class SerializerExceptions
+    {
+        public class SerializationException : Exception
+        {
+            public SerializationException() : base("Serialization failed!") { }
+            public SerializationException(string message) : base(message) { }
+        }
+
+        public class DeserializationException : SerializationException { }
+    }
+}
