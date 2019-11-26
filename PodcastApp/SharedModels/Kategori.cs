55c504dfb2c71d84d0ddd6d514b604a8780d7cae
SNDL-philip <philipsandell7@hotmail.com>
2019-11-26 11:03:35 +0100

    funkar inte
    funkar inte lista

--- PodcastApp/SharedModels/Kategori.cs
+++ PodcastApp/SharedModels/Kategori.cs
@@ -6,18 +6,34 @@ using System.Threading.Tasks;
 
 namespace SharedModels
 {
-        public class Kategori
-        {
+    public class Kategori
+    {
         public string KategoriNamn { get; set; }
         public Kategori(string kategorinamn)
-            {
-                this.KategoriNamn = kategorinamn;
-            }
+        {
+            this.KategoriNamn = kategorinamn;
+        }
         public Kategori()
         {
 
         }
+        public static List<Kategori> AddkategoriToList(string Kategorinamnet)
+        {
+            List<Kategori> kategorilista = new List<Kategori>();
+
+            foreach (kategorinamnet in kategorilista)
+            {
+                Kategori kategorin = new Kategori();
+
+                count++;
+                nypodd.Title += item.Title.Text; //funkar
+                nypodd.Url += item.Links[0].Uri.OriginalString; //funkar
+                nypodd.Description = item.Summary.Text;
+                nypodd.Kategori = nypodd.getKategori(); //funkar men tar med <p> taggar
+                podcastlista.Add(nypodd);
+
+            }
+        }
     }
 }
-
 
