0cd1c83244483fece49413806ac62c76a2cdebc1
Mattias Bolander <55517764+TiasB@users.noreply.github.com>
2019-12-03 16:59:05 +0100

    Med validering

---
+++ PodcastApp/Logic/Validate/Validation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Validate
+{
+    class Validation
+    {
+        public static bool isEmpty(string text)
+        {
+            bool result = false;
+
+            if (text.Equals("") || text.Equals(null))
+            {
+                result = false;
+            }
+            else
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+
+    }
+}
