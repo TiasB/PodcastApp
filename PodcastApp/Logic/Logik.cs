fb97f0a32b6ea38b2de2aedb6b98a2aab4b4cd45
SNDL-philip <philipsandell7@hotmail.com>
2019-12-03 15:15:25 +0100

    Update Logik.cs

--- PodcastApp/Logic/Logik.cs
+++ PodcastApp/Logic/Logik.cs
@@ -6,12 +6,37 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using Data;
+
 using SharedModels;
 
 namespace Logic
 {
-    public class Logic
+    public class Logik
     {
-    }
+        public List<Kategori> allaKategorier { get; set; }
+
+
+        public Logik()
+        {
+            allaKategorier = new List<Kategori>();
+
+        }
+
+        public void nyKategori(string Kategorinamn)
+        {
+            Kategori enKategori = new Kategori(Kategorinamn);
+            läggTillKategori(enKategori);
+            
+        }
+        public void läggTillKategori(Kategori nykategori)
+        {
+            allaKategorier.Add(nykategori);
+        }
+        public void bajs(string bajs)
+        {
+            Console.WriteLine(bajs);
+        }
+    }//logik
+
 }
 
